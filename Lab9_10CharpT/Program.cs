5487336339c82790ca412caa4985fed718725bf6
ValeriiSanduliak <112845911+ValeriiSanduliak@users.noreply.github.com>
2024-03-01 13:37:37 +0200

    Task1_done

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿using System;
+using System.Collections.Generic;
+using Lab9_10CharpT;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lab 9 CSharp");
+
+        while (true)
+        {
+            Console.WriteLine("=========================================================");
+            Console.WriteLine("Select a task:");
+            Console.WriteLine("1. Task 1");
+            Console.WriteLine("2. Task 2");
+            Console.WriteLine("3. Task 3");
+            Console.WriteLine("4. Task 4");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("=========================================================");
+            Console.Write("Enter your choice >>> ");
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Task1.Task1_();
+                    break;
+                /*  case "2":
+                      Task2.Task2_();
+                      break;
+                  case "3":
+                      Task3.Task3_();
+                      break;
+                  case "4":
+                      Task4.Task4_();
+                      break;*/
+
+                case "5":
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab9_10CharpT/Task1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_10CharpT
+{
+    public class Task1
+    {
+        public static void Task1_()
+        {
+            string input = "abc#d##c   a#bc#d#c    abc##d";
+            Console.WriteLine("Input string : ");
+            Console.WriteLine(input);
+            Console.WriteLine("Result : ");
+            string result = ProcessInput(input);
+            Console.WriteLine(result);
+        }
+
+        static string ProcessInput(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '#')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            char[] resultArray = stack.ToArray();
+            Array.Reverse(resultArray);
+            string result = new string(resultArray);
+
+            return result;
+        }
+    }
+}
